39d7cf8f8b6369542feca16d0868b41b7ec11bf7
Lucas <lucaslopesgrisolia@gmail.com>
2026-05-28 14:48:55 -0300

    ajustes footer e header e criação do banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <>
     <Header />
+    <Banner />
     <h1>Hello World</h1>
     <p>Estou aprendendo React JS</p>
     <Footer />
