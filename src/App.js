a2fcb53a38a6c8e3675a969229ba689092141a09
Lucas <lucaslopesgrisolia@gmail.com>
2026-05-29 17:31:14 -0300

    Criação do container ainda não preenchido e alteração do banner para props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Banner from "./components/Banner";
+import Container from "./components/Container";
 
 function App() {
   return (
     <>
     <Header />
-    <Banner />
-    <h1>Hello World</h1>
-    <p>Estou aprendendo React JS</p>
+    <Banner image="home" />
+    <Container>
+      <h1>Hello World</h1>
+      <p>Estou aprendendo React JS</p>
+    </Container>
     <Footer />
     </>
   );
